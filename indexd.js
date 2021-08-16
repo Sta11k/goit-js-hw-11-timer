b3d3c8d4dc77be4aacf748681aec81ca1d125c32
Sta11k <Alexandiel1984@gmail.com>
2021-08-16 07:43:15 +0300

    timer

--- indexd.js
+++ indexd.js
@@ -28,3 +28,49 @@ const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
  * миллисекунд в одной секунде (1000)
  */
 const secs = Math.floor((time % (1000 * 60)) / 1000);
+// stopwatch
+
+// class Stopwatch {
+//   constructor() {
+//     this.timerId = null;
+//     this.dateStart = null;
+//     this.startBtn = document.querySelector("#start");
+//     this.stopBtn = document.querySelector("#stop");
+//     this.sec = document.querySelector(".seconds");
+//     this.min = document.querySelector(".minutes");
+//     this.hours = document.querySelector('.hours')
+
+//     this.action = this.action.bind(this);
+//     this.start = this.start.bind(this);
+//     this.stop = this.stop.bind(this);
+//   }
+
+//   action() {
+//     const delta = Date.now() - this.dateStart;
+//     //   console.log(delta);
+//     const sec = Math.floor((delta / 1000) % 60);
+//     const minutes = Math.floor((delta / 1000 / 60) % 60);
+//     const hours = Math.floor((delta / 1000 / 60 / 60) % 24);
+
+//     this.sec.textContent = sec < 10 ? `0${sec}` : sec;
+//     this.min.textContent = minutes < 10 ? `0${minutes}`: minutes;
+//     this.hours.textContent = hours < 10 ? `0${hours}` : hours
+//   }
+
+//   start() {
+//     this.timerId = setInterval(this.action, 500);
+//     this.dateStart = Date.now();
+//   }
+
+//   stop() {
+//     clearInterval(this.timerId);
+//   }
+
+//   init() {
+//     this.startBtn.addEventListener("click", this.start);
+//     this.stopBtn.addEventListener("click", this.stop);
+//   }
+// }
+
+// const watch = new Stopwatch();
+// watch.init();
